b9c224e376a47b943e451875cf5927b9b42c1cba
Alejandro19200 <71737683+Alejandro19200@users.noreply.github.com>
2020-09-23 20:19:51 -0500

    Programa
    Segundo programa c#

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nombre;
+            String ApellidoPaterno;
+            String ApellidoMaterno;
+            int Edad;
+            String Ocupacion;
+            String Correo;
+            String Mes;
+
+            Console.WriteLine("REGISTRO DE PLATAFORMA");
+            Console.WriteLine("Ingresa Tu nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine(Nombre);
+
+            Console.WriteLine("Ingrese su apellido Paterno");
+            ApellidoPaterno = Console.ReadLine();
+            Console.WriteLine(ApellidoPaterno);
+
+            Console.WriteLine("Ingrese su apellido Materno");
+            ApellidoMaterno = Console.ReadLine();
+            Console.WriteLine(ApellidoMaterno);
+
+            Console.WriteLine("Ingresa tu edad: ");
+            Edad = Convert.ToInt32(Console.ReadLine());
+            if (Edad <= 18)
+            {
+                Console.WriteLine("El usuario es menor de edad, Registro Fallido");
+            }
+
+            Console.WriteLine("Ingresa su ocupación ");
+            Ocupacion = Console.ReadLine();
+            Console.WriteLine(Ocupacion);
+
+            Console.WriteLine("Ingresa su correo electronico ");
+            Correo = Console.ReadLine();
+            Console.WriteLine(Correo);
+
+        }
+    }
+}
